cfbc1550fafc8b0df486495ba4107a1d4a5b5c8c
Rafael-Creator-max <rafael.pereztarira@student.kuleuven.be>
2026-06-19 17:50:30 +0200

    Initial tests

--- src/test.py
+++ src/test.py
@@ -3,11 +3,16 @@ import pandas as pd
 print("Pandas werkt!")
 print(pd.__version__)
 
+print("--------------------------------")
+
+
 df = pd.read_csv("../data/monthly_hourly_load_values_2019.csv",sep="\t")
 
 print(df.head())
 print("\nKolommen:")
 print(df.columns)
+
+print("--------------------------------")
 
 print("\nInfo:")
 print(df.info())
@@ -17,5 +22,37 @@ with open("../data/monthly_hourly_load_values_2019.csv", "r", encoding="utf-8") 
 
 print(repr(line))
 
+print("--------------------------------")
+
+#Check the timeseries timeline
+
 print(df["CountryCode"].unique())
+
+be_df = df[df["CountryCode"] == "BE"]
+print(be_df["DateUTC"].min())
+print(be_df["DateUTC"].max())
+
+#Convert datum
+
+be_df["DateUTC"] = pd.to_datetime(
+    be_df["DateUTC"]
+    ,format="%d-%m-%Y %H:%M"
+)
+
+print(be_df["DateUTC"].min())
+
+print("--------------------------------")
+
+#First quality control
+
+
+#Check missing values
+print("Missing values")
+print(be_df.isnull().sum())
+
+#Check duplicates
+print("Duplicates")
+print(be_df.duplicated().sum())
+
+
 
